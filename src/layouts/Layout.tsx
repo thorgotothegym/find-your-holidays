454550146ed9d1d2d5dbb5891e7bf4e109066cd1
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-14 13:55:29 +0100

    fix: styling and remove button from layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -7,9 +7,6 @@ type Layout = {
 export const Layout = ({ children }: Layout) => {
   return (
     <div className="relative flex min-h-svh flex-col bg-background">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white">
-        halo alo alo
-      </button>
       {/* Sidebar */}
       <aside className="w-60 bg-gray-800 p-4 text-white">
         <nav></nav>
