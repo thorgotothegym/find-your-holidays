debdce3fa58ae6dacb69c66c674aaefaf5f1a13c
Aleix Vidal <al3ix.vidal@gmail.com>
2025-02-17 11:38:13 +0100

    feat: update Tailwind CSS configuration and enhance styling with custom CSS variables

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,9 +6,9 @@ type Layout = {
 
 export const Layout = ({ children }: Layout) => {
   return (
-    <div className="relative flex min-h-svh flex-col bg-background">
+    <div className="relative flex min-h-svh flex-col bg-current text-foreground">
       {/* Sidebar */}
-      <aside className="w-60 bg-gray-800 p-4 text-white">
+      <aside className="w-60 p-4 text-white">
         <nav></nav>
       </aside>
       <main className="flex-1 p-6 bg-gray-100">{children}</main>
